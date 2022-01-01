b51a68dc23dece8c547f26708af591713fe84acd
Shahrukh <themrsrk@gmail.com>
2022-01-01 23:06:16 +0500

    Completed Assignment no: 1

---
+++ Assignment # 1.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#Program no:1
+
+print ("Twinkle Twinkle Little Start\n\t How I wonder what you are! \n\t\t Up Above the world so high \n\t\t Like a Diamond in the Sky \n Twinkle Twinkle Little Star \n\t How I wonder What you are")
+
+
+# In[6]:
+
+
+#Program no:2 
+
+import sys
+print(sys.version)
+
+
+# In[4]:
+
+
+#Program no: 3
+
+import datetime
+current = datetime.datetime.now()
+print(current)
+
+
+# In[3]:
+
+
+#Program no: 4
+
+radius = int(input("Enter the radius of circle: "))
+pi = 3.14159265
+result = pi * (radius**2)
+print("The Area of a circle is: ", result)
+
+
+# In[7]:
+
+
+#Program no: 5
+
+first = input("Enter your first name: ")
+last = input("Enter your last name: ")
+print( last, " ",first)
+
+
+# In[8]:
+
+
+#Program no: 4
+first_num = int(input("Enter first number: "))
+last_num = int(input("Enter second number: "))
+print ("The addition of both the numbers are: ",first_num + last_num)
+
+
+# In[ ]:
+
+
+
+
